c584e22775bc06aaae5c8a54fcaaee821dae23bb
keoshayoung <keoshayoung@email.com>
2022-07-15 16:31:00 -0400

    created update action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,13 @@ class UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }, status: :bad_request
     end
   end
+
+  def update
+    user = User.find_by(id: params[:id])
+    user.name = params[:name] || user.name
+    user.email = params[:email] || user.email
+    user.admin = params[:admin] || user.admin
+    user.save
+    render json: user.as_json
+  end
 end
